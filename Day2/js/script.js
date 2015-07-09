2220e8234b6533a8de129f5cf8fadaabd61aa005
lucasacw05 <lucasacw@gmail.com>
2015-07-09 19:18:28 -0400

    Created starting files for day 2
    Created files in WebStorm

---
+++ Day2/js/script.js
@@ -0,0 +1,21 @@
+/*
+This is going to be a multi lined comment
+Lucas Alencar
+7-9-2015
+SDI Section 00
+Day 2 Coding Basics
+ */
+
+//alert("This is making sure our js file is connected!");
+
+//This is a single lined comment
+//Usually will describe what comes right after
+
+/*
+Multi-line comment
+Anything between the stars and slashes is ignored by the browsers
+ */
+
+//alert - gives the user information but returns nothing to us
+
+alert("Welcome to SDI!");

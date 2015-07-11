f9688f847d96a766ae234e2baa348148875a9a69
lucasacw05 <lucasacw@gmail.com>
2015-07-11 19:33:17 -0400

    Covered Prompts
    Covered Prompts

--- Day3/js/day3_prompts.js
+++ Day3/js/day3_prompts.js
@@ -7,3 +7,28 @@ Prompts
 
 //alert("Test It")
 
+//Ask the user for their input
+var userInput = prompt("Enter your year of birth");
+console.log(userInput)
+
+var age = (2015-userInput)-1;
+console.log("You are "+age+" years old.");
+alert("You are "+age+"years old.");
+
+//Calculate the area of a rectangle
+
+//Prompt the user for width
+var width = prompt("Let's calculate the area of a rectangle.\nPlease enter a width:");
+
+//Prompt the user for a height
+var height = prompt("Please enter a height");
+
+//Calculate the area using the user input
+var area = width * height;
+console.log("The area of your rectangle is "+area);
+
+//Create variable that holds the whole result
+
+var result = "The area of your rectangle is "+area;
+console.log(result);
+alert(result);

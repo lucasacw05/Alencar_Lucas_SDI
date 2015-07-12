b68c78d2810bd014b1d5eecb948721ce9d8be83c
lucasacw05 <lucasacw@gmail.com>
2015-07-11 21:57:30 -0400

    Expressions Assignment Just started.
    Expressions Assignment Just started.

---
+++ Alencar_Lucas_Expressions_Assignment/js/day3_expressionsassignment.js
@@ -0,0 +1,12 @@
+
+/*
+ Lucas Alencar
+ Section 00
+ 7-11-2015
+ Expressions Assignment
+ */
+
+alert("This is Lucas Alencar's Expressions Assignment. Enjoy it!");
+
+//This is the starting point of the Expressions Assignment started on Day 3.
+

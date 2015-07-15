5e9134508677351ad636fc8213f5e9a446c781a5
lucasacw05 <lucasacw@gmail.com>
2015-07-15 00:52:54 -0400

    3rd Problem Started
    3rd Problem Started

--- Day 4 Lab/js/conditionals_worksheet.js
+++ Day 4 Lab/js/conditionals_worksheet.js
@@ -119,7 +119,7 @@ Conditionals Worksheet
    alert("Password does not match our records");
   }
  /*By last, if you put the correct username and password, you will receive the following message: "Congratulations, you just logged in!"
- 
+
   */
   if ((question1 === userName) && (question2 === password)) {
    console.log("Congratulations, you just logged in!");
@@ -127,6 +127,23 @@ Conditionals Worksheet
   }
 
  }
+
+
+ /*Movie Ticket Price
+
+ The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older),
+ under 10 you get the discounted price of $7.00. In addition, if a customer is seeing a movie between
+ 3pm and 5pm they can also get the discounted price.
+
+     Determine which of the two prices the customer is eligible for.
+
+ Given:
+     Time of Movie (Assume whole numbers here)
+ Age of the customer
+ Result To Print Out:
+     “The ticket price is X”
+     */
+
 
 
 

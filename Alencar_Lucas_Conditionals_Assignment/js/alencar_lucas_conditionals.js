3fa37c9a97995e84902de7374bacc3432e6c4e0f
lucasacw05 <lucasacw@gmail.com>
2015-07-17 01:04:31 -0400

    Ternaries almost finished.
    Ternaries almost finished.

--- Alencar_Lucas_Conditionals_Assignment/js/alencar_lucas_conditionals.js
+++ Alencar_Lucas_Conditionals_Assignment/js/alencar_lucas_conditionals.js
@@ -91,10 +91,13 @@ Conditionals Assignment
 
 
  personName=prompt("What is your first name?");
+ personName = (personName==="")? prompt("This is not a person's name.\nWhat is your name?") :personName;
 
  personLastName=prompt("And your last name?");
+ personLastName= (personLastName==="")? prompt("This is not a person's name.\nWhat is your name?");
 
  personAge=prompt("How old are you?");
+ personAge = (personAge==="")? prompt("This is no");
 
 
 
@@ -102,10 +105,16 @@ Conditionals Assignment
  if (personAge<=17){
   console.log("Hello "+personName+" "+personLastName+"! It looks like you are still a baby boy!");
   alert("Hello "+personName+" "+personLastName+"! It looks like you are still a baby boy!");
- } else if (personAge>=21 && personAge<=25){
+ }
+
+
+ else if (personAge>=21 && personAge<=25){
   console.log("So you already are a a grown man or woman and was born in the 90\'s. That is great "+personName+"!!");
   alert("So you already are a a grown man or woman and was born in the 90\'s. That is great "+personName+"!!");
- } else if (personAge>=26 || personAge>26){
+ }
+
+
+ else if (personAge>=26 || personAge>26){
   console.log("I know you lived back in the 80's! I even think that you enjoyed the LPs era, but let's calculate your CDs!!");
   alert("I know you lived back in the 80's! I even think that you enjoyed the LPs era, but let's calculate your CDs!!");
  }
@@ -115,9 +124,10 @@ Conditionals Assignment
  timeBuyingCds =  (personAge-ageStartedBuyingCds);
 
  alert("Then you have been buying CDs for incredible "+timeBuyingCds+" years!");
+ console.log("Then you have been buying CDs for incredible "+timeBuyingCds+" years!");
 
  alert("Let's make things more interesting...");
-
+console.log("Let's make things more interesting...");
 
 
  cdprice=prompt("How much do you usually pay per CD since you were "+ageStartedBuyingCds+" and started buying it?");
@@ -137,6 +147,8 @@ Conditionals Assignment
   alert("Please, answer again making sure to put only numbers.");
  } else {
   alert("Great! That means that you buy an average of "+ cdsPerMonth+" CDs per month and "+cdsPerYear+" CDs per year, making your collection to be around "+cdsCollection+" CDs today!!");
+  console.log("Great! That means that you buy an average of "+ cdsPerMonth+" CDs per month and "+cdsPerYear+" CDs per year, making your collection to be around "+cdsCollection+" CDs today!!");
+
  }
 
  moneySpentOnCollection = cdsCollection * cdprice;
@@ -144,11 +156,18 @@ Conditionals Assignment
  painfulPart=prompt("Now it's time for the painful part, are you prepared? (Answer yes or no.)");
 
  if (painfulPart.toLowerCase()==="yes"){
-  alert("Through all these "+timeBuyingCds+" years buying CDs, you spent an entire amount of $"+moneySpentOnCollection+" dollars. ")
- } else if ((painfulPart.toLowerCase()==="no") || (painfulPart==="")){
-  alert("It looks you are not brave enough...")
- } else {
+  alert("Through all these "+timeBuyingCds+" years buying CDs, you spent an entire amount of $"+moneySpentOnCollection+" dollars. ");
+  console.log("Through all these "+timeBuyingCds+" years buying CDs, you spent an entire amount of $"+moneySpentOnCollection+" dollars. ");
+ }
+
+ else if ((painfulPart.toLowerCase()==="no") || (painfulPart==="")){
+  alert("It looks you are not brave enough...");
+  console.log("It looks you are not brave enough...");
+ }
+
+ else {
   alert("You'd better remain in silent... You could have had a heart attack!");
+  console.log("You'd better remain in silent... You could have had a heart attack!");
  }
 
 

744a6b8df9bda833b7624425349a3efdf6623c41
lucasacw05 <lucasacw@gmail.com>
2015-07-16 23:25:29 -0400

    I'm Still in progress.
    Everything looks to start working, but I still need to set up the
    calculator.

--- Alencar_Lucas_Conditionals_Assignment/js/alencar_lucas_conditionals.js
+++ Alencar_Lucas_Conditionals_Assignment/js/alencar_lucas_conditionals.js
@@ -22,6 +22,14 @@ Conditionals Assignment
 
  var cdprice;
 
+ var cdsPerWeek;
+
+ var timeBuyingCds;
+
+ var cdInfo;
+
+
+
  //This is the cost of brand new CDs
  var brandNewCdsCost;
 
@@ -31,11 +39,14 @@ Conditionals Assignment
 
  var personAge;
 
- var personStatistics;
+ var ageStartedBuyingCds;
 
- var gender;
+ var personStatistics;
+ 
 
  personStatistics = [personName, personLastName, personAge];
+
+ cdInfo = [cdprice, cdsPerWeek, timeBuyingCds, ageStartedBuyingCds];
 
 
  //This first part is an introduction part to not to make the calculator so boring.
@@ -48,12 +59,15 @@ Conditionals Assignment
   */
  if(firstQuestion.toLowerCase()==="yes"){
   console.log("Great! Let's start!");
+  alert("Great! Let's start");
  } else if(firstQuestion===""){
    console.log("I know you were lazy to answer... It doesn't matter, we will continue.");
+  alert("I know you were lazy to answer... It doesn't matter, we will continue.");
  }
 
  else {
-  console.log("It doesn't matter, we will continue.")
+  console.log("It doesn't matter, we will continue.");
+  alert("It doesn't matter, we will continue.");
  }
 
 
@@ -63,16 +77,23 @@ Conditionals Assignment
 
  personAge=prompt("How old are you?");
 
+ timeBuyingCds =  (personAge-ageStartedBuyingCds);
+
 
  if (personAge<=17){
   console.log("Hello "+personName+" "+personLastName+"! It looks like you are still a baby boy!");
   alert("Hello "+personName+" "+personLastName+"! It looks like you are still a baby boy!");
  } else if (personAge>=21 && personAge<=25){
   console.log("So you already are a a grown man or woman and was born in the 90\'s. That is great "+personName+"!!");
+  alert("So you already are a a grown man or woman and was born in the 90\'s. That is great "+personName+"!!");
  } else if (personAge>=26 || personAge>26){
-  console.log("I know you lived back in the 80's! I even think that you enjoyed the LPs era, but let's calculate your CDs!!")
+  console.log("I know you lived back in the 80's! I even think that you enjoyed the LPs era, but let's calculate your CDs!!");
+  alert("I know you lived back in the 80's! I even think that you enjoyed the LPs era, but let's calculate your CDs!!");
  }
 
+ ageStartedBuyingCds=prompt("What was you age when you started buying CDs?");
+
+ alert("Then you have been buying CDs for incredible "+timeBuyingCds+" years!");
 
 
 

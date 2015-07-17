f262180ecd8d19f550075a36af25dd8a5221686b
lucasacw05 <lucasacw@gmail.com>
2015-07-16 22:15:51 -0400

    First Upload of Conditionals Assignment

---
+++ Alencar_Lucas_Conditionals_Assignment/js/alencar_lucas_conditionals.js
@@ -0,0 +1,12 @@
+
+ /*
+Lucas Alencar
+Section 00 
+7-16-15
+Conditionals Assignment
+ */
+
+alert("This is Lucas Alencar's Conditionals Assignment");
+
+ //This is the beginning of Lucas Alencar's Conditionals Assignment.
+ 

e822c96fddf3750503521189b078f48a7e2d0ba5
lucasacw05 <lucasacw@gmail.com>
2015-07-21 14:55:13 -0400

    New loop worksheet.
    I created another loop worksheet because the first one was not good.

---
+++ Alencar_Lucas_Loops_Worksheet/js/loops_worksheet2.js
@@ -0,0 +1,32 @@
+
+ /*
+Lucas Alencar
+Section 00 
+7-21-2015
+Loops Worksheet
+ */
+
+//alert("Test It");
+
+ var firstName;
+ var lastName;
+ var dayOfBirth;
+ var monthOfBirth;
+ var yearOfBirth;
+ var userName;
+ var profile;
+
+ firstName=prompt("Let's create your profile for this brand new social media!\nInsert here your first name:");
+
+ lastName=prompt("Now, it is time for your last name:");
+
+ dayOfBirth=prompt("In what day of the month you were born?");
+
+ monthOfBirth=prompt("What is your month of birth?");
+
+ yearOfBirth=prompt("Take it easy, we are almost at the end.\n In what year you were born?");
+
+ userName=prompt("By last, what username would you like to use?");
+
+
+ alert("This is your brand new profile!\nYour name is "+firstName+" "+lastName+"\nYou were born in "+monthOfBirth+"/"+dayOfBirth+"/"+yearOfBirth+" and your new username is "+userName+".");

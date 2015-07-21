c3dfdbfdec34ea8f5b7b7318b9866c685e96c945
lucasacw05 <lucasacw@gmail.com>
2015-07-21 19:02:11 -0400

    Zombie Attack Finished!
    Zombie Attack Finished!

--- Day7/js/day7_zombies.js
+++ Day7/js/day7_zombies.js
@@ -22,6 +22,8 @@ Zombie Attack!
  //What is our timeline?
  var days = 8;
 
+
+ /*
  for(var i=1; i<=days; i++){
   //How many people get infected every day?
   var infectedPeople = numZombies*numBites;
@@ -30,3 +32,28 @@ Zombie Attack!
   numZombies=numZombies+=infectedPeople;
   console.log("On day #"+i+" there will be "+numZombies+" zombies!");
  }
+ */
+
+ //The CDC wants to know how many days it will take for their to be a million zombies.
+
+ //Variable to count the number of days
+ var numDays = 1;
+
+ while(numZombies<=1000000){
+
+   //How many people get infected every day?
+   var infectedPeople = numZombies*numBites;
+
+   //How many zombies will we have?
+   numZombies=numZombies+=infectedPeople;
+
+  //Report out each day
+   console.log("On day #"+numDays+" there will be "+numZombies+" zombies!");
+
+  //Increment the day
+  numDays++;
+
+ }
+
+ console.log("It will take "+(numDays-1)+" days to make a million zombies!");
+

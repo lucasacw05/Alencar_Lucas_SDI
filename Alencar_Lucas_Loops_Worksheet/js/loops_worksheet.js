093cd8a89f60a196149d3526e123bee9c4dc7f66
lucasacw05 <lucasacw@gmail.com>
2015-07-21 08:00:25 -0400

    Code update.
    The code is running but there is a few problem that I’m trying to solve.

--- Alencar_Lucas_Loops_Worksheet/js/loops_worksheet.js
+++ Alencar_Lucas_Loops_Worksheet/js/loops_worksheet.js
@@ -1,7 +1,7 @@
 
  /*
 Lucas Alencar
-Section 00 
+Section 00
 7-20-2015
 Loops Worksheet
  */
@@ -20,38 +20,39 @@ Loops Worksheet
  var firstQuestion;
 
  firstQuestion=prompt("Are you the person who wants to order pizzas for the zombie apocalypse, right?");
+if(firstQuestion.toLowerCase() === "yes"){
 
- if(firstQuestion.toLowerCase() === "yes") {
+ pizzasOrdered=prompt("How many pizzas would you like to order to stock for the zombie apocalypse?");
+ if(pizzasOrdered > 30){
+  alert("Great! It looks like you really want to survive!");
+  moneyInWallet=prompt("Great, how much money do you have in your wallet? The cost of each pizza is $4");
+  if(moneyInWallet >(pizzasOrdered * pizzaPrice)){
+   alert("It looks like you have enough money my friend, you are good to go.");
+   alert("Here, take your order.");
+  } else {
+   alert("I'm sorry, but there is no pizza for you");
+  }
 
-  pizzasOrdered = prompt("How many pizzas would you like to order to stock for the zombie apocalypse?");
-  if (pizzasOrdered > 30) {
-   alert("Great! It looks like you really want to survive!");
-   moneyInWallet = prompt("Great, how much money do you have in your wallet? The cost of each pizza is $4");
-   if (moneyInWallet > (pizzasOrdered * pizzaPrice)) {
-    alert("It looks like you have enough money my friend, you are good to go.");
-    alert("Here, take your order.");
-   } else {
-    alert("I'm sorry, but there is no pizza for you");
-   }
+
+
+ }
+
+
+   else
+ for(pizzasOrdered; pizzasOrdered < 31; pizzasOrdered++){
+  alert("I still think that this is not enough... We only accept orders of 30 or more pizzas. We help you survive, not to have fun. Let's help you survive adding 1 pizza per time to your order until you have at least 30. Be calm, imagine how many orders we do have. Until now you have "+pizzasOrdered+" pizzas.");
+ if (pizzasOrdered === 30){
+  alert("I think that now it is enough.");
+  break;
+ } moneyInWallet=prompt("Great, how much money do you have in your wallet? The cost of each pizza is $4");
+  if(moneyInWallet >(pizzasOrdered * pizzaPrice)){
+   alert("It looks like you have enough money my friend, you are good to go.");
+   alert("Here, take your order.");
+  } else {
+   alert("I'm sorry, but there is no pizza for you");
   }
  }
 
-   else {
-  for (pizzasOrdered; pizzasOrdered < 31; pizzasOrdered++) {
-   alert("I still think that this is not enough... We only accept orders of 30 or more pizzas. We help you survive, not to have fun. Let's help you survive adding 1 pizza per time to your order until you have at least 30. Be calm, imagine how many orders we do have. Until now you have " + pizzasOrdered + " pizzas.");
-   if (pizzasOrdered === 30) {
-    alert("I think that now it is enough.");
-    break;
-   }
-   moneyInWallet = prompt("Great, how much money do you have in your wallet? The cost of each pizza is $4");
-   if (moneyInWallet > (pizzasOrdered * pizzaPrice)) {
-    alert("It looks like you have enough money my friend, you are good to go.");
-    alert("Here, take your order.");
-   } else {
-    alert("I'm sorry, but there is no pizza for you");
-   }
-  }
- }
 
 
 
@@ -65,11 +66,17 @@ Loops Worksheet
   firstQuestion = prompt("Please, answer \"yes\" or \"no\".\nAre you the person who wants to order pizzas for the zombie apocalypse?");
  if(firstQuestion.toLowerCase() === "no"){
   alert("Never mind, you might be the wrong person.");
- } else if(firstQuestion.toLowerCase() === "yes"){
+ }
 
-  moneyInWallet=prompt("Great, how much money do you have in your wallet");
+ else if(firstQuestion.toLowerCase() === "yes"){
+  moneyInWallet=prompt("Great, how much money do you have in your wallet? The cost of each pizza is $4");
+  if(moneyInWallet >(pizzasOrdered * pizzaPrice)){
+   alert("It looks like you have enough money my friend, you are good to go.");
+   alert("Here, take your order.");
+  } else {
+   alert("I'm sorry, but there is no pizza for you");
+  }
 
-  pizzasOrdered=prompt("How many pizzas would you like to order to stock for the zombie apocalypse?");
  }
 
  }

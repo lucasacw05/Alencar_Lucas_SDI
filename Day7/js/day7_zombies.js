81cae71f39c33c3d9bb530cd1c55a3461338b0d7
lucasacw05 <lucasacw@gmail.com>
2015-07-21 18:49:19 -0400

    For Loop version for 8 days.
    For Loop version for 8 days.

--- Day7/js/day7_zombies.js
+++ Day7/js/day7_zombies.js
@@ -6,4 +6,27 @@ Section 00
 Zombie Attack!
  */
 
-alert("Test It");
+//alert("Test It");
+
+ //We have 1 zombie here at Full Sail.
+ //It can bite 4 people a day and turn them into Zombies by the next day.
+ //The CDC wants to know how many zombies it will be in 8 days.
+
+ //Create the givens
+ //How many zombies we have
+ var numZombies = 1;
+
+ //Number of bites a zombie can make each day
+ var numBites = 4;
+
+ //What is our timeline?
+ var days = 8;
+
+ for(var i=1; i<=days; i++){
+  //How many people get infected every day?
+  var infectedPeople = numZombies*numBites;
+
+  //How many zombies will we have?
+  numZombies=numZombies+=infectedPeople;
+  console.log("On day #"+i+" there will be "+numZombies+" zombies!");
+ }

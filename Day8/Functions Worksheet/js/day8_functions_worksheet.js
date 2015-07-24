cf5229dd54593b15562fba67c343b2c9b47836a4
lucasacw05 <lucasacw@gmail.com>
2015-07-23 21:05:44 -0400

    Just started the first problem
    Just started the first problem

--- Day8/Functions Worksheet/js/day8_functions_worksheet.js
+++ Day8/Functions Worksheet/js/day8_functions_worksheet.js
@@ -8,4 +8,26 @@ Functions Worksheet Activity
 
 //alert("This is Functions Worksheet");
 
- 
+ //Circumference
+
+ /*Calculate the circumference of a circle.
+
+     Parameter(s) for function:
+ Radius of the circle
+ Return:
+     Circumference of the circle
+ Result to print to the console:
+     “The circumference of the circle is X”;
+*/
+
+ function calcCirc(){
+
+  var r;
+  var circumference = 2*Math.PI*r;
+
+  
+  console.log("The circumference of the circle with the radius of "+r+" is "+circumference);
+
+ }
+
+

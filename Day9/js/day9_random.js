2bef5f09d50643d89ca50ee52c4978589f1590c7
lucasacw05 <lucasacw@gmail.com>
2015-07-25 17:54:09 -0400

    Prompted and validated max
    Prompted and validated max

--- Day9/js/day9_random.js
+++ Day9/js/day9_random.js
@@ -26,3 +26,29 @@ alert("Test It");
  //Number cast or parseInt
 
  min=Number(min);
+
+ //Variable for max
+
+ var max=prompt("What is the max value?");
+
+ //Validate the prompt
+
+
+ max = prompt("We are going to find a random number between two values.\nWhat is the minimum value?");
+ //Validate that prompt.
+ while((isNaN(max) || max==="") ||max<=min){
+
+  if(isNaN(max)){
+   max=prompt("Please, only type in numbers and what is the minimum value?");
+  } else if(max<=min && max!=""){
+   max=prompt("Please make sure the max value is greater than the min value of "+min+"\nWhat is the max value?");
+  }
+  else{
+   max=prompt("Please, do not leave blank.\nWhat is the min value?");
+  }
+
+ }
+
+
+//Number cast max
+ max = Number(max);

c5473e67d051dc4a0c88ffcd9fc3bee447960002
lucasacw05 <lucasacw@gmail.com>
2015-07-26 01:35:41 -0400

    All problems finished.
    All problems finished. I just need to comment and set up the consoles.

--- Alencar_Lucas_Functions_Assignment/js/alencar_lucas_functions_assignement.js
+++ Alencar_Lucas_Functions_Assignment/js/alencar_lucas_functions_assignement.js
@@ -26,79 +26,91 @@ var firstQuestion=prompt("Did you know that the internet that you use everyday i
 
  while(secondQuestion === "" || !(isNaN(secondQuestion))){
   secondQuestion=prompt("Please, type only \"yes\" or \"no\" and don't leave it blank.")
- } if(secondQuestion.toLowerCase()==="yes"){
+ } if(secondQuestion.toLowerCase()==="yes") {
   alert("Great! we will do this because you want to and because this idea is awesome!!");
- } else{
-  alert("It looks like you will have to do it in any way! Hahaha");
+
+
+  alert("Before we start, if we were going to do everything as it really is in real life, things could start to be boring.");
+
+  radius = prompt("By first, choose the size of the radius in meters.");
+  while (radius === "" || isNaN(radius)) {
+   radius = prompt("Please, type only type numbers and don't leave it blank.")
+  }
+  if (!(isNaN(radius))) {
+   alert("Awesome choice!!");
+  }
+
+
+  length = prompt("By last, choose its length.");
+  while (length === "" || isNaN(length)) {
+   length = prompt("Please, type only type numbers and don't leave it blank.")
+  }
+  if (!(isNaN(length))) {
+   alert("Soon you will see the magic number!");
+  }
+
+
+  var totalVolume = function (r, l) {
+
+   var areaCircumference = r * r * Math.PI;
+   var volume = areaCircumference * l;
+   console.log(volume);
+   return volume;
+  };
+
+
+
+  var resultVolume = totalVolume(radius, length);
+
+  alert("The total volume of your own wonderful version of the Amazing Internet Cable is " + Math.round(resultVolume) + " cubic meters!!!");
+
+  alert("There is definitely more than were it came from!");
+
+  alert("After so hard calculations, let's pretend your are going to eat an ice cream!\nThe ice cream you want to eat comes not inside a cone, but a pyramid! Let's calculate its volume! What a fun thing to do!");
+
+
+  var width2 = prompt("Let's start with the width of the pyramid. Type only numbers.");
+  while (width2 === "" || isNaN(width2)) {
+   width2 = prompt("Please, type only type numbers and don't leave it blank.")
+  }
+  if (!(isNaN(width2))) {
+   alert("Great!!!");
+  }
+
+
+  var length2 = prompt("Now it's time for the length! Type here again. You know what you need to do.");
+  while (length2 === "" || isNaN(length2)) {
+   length2 = prompt("Please, type only type numbers and don't leave it blank.")
+  }
+  if (!(isNaN(length2))) {
+   alert("You love Ice Cream, don't you?");
+  }
+
+
+  var height2 = prompt("By last, type the height of your pyramid");
+  while (height2 === "" || isNaN(height2)) {
+   height2 = prompt("Please, type only type numbers and don't leave it blank.")
+  }
+  if (!(isNaN(height2))) {
+   alert("YEAAAHHH!");
+  }
+
+
+  function calcPyramid(w, l, h) {
+   var base = w * l;
+   var volume = base * (h / 3);
+   console.log(volume);
+   return volume;
+  }
+
+  var resultVolume2 = calcPyramid(width2, length2, height2);
+
+  alert("The volume of your Ice Cream \"cone\" that is a Pyramid is " + Math.round(resultVolume2) + " cubic in the unit you wanted to be. Remember I only asked you the numbers. Have fun with Code and with Life! Thanks!");
+
  }
 
-
-alert("Before we start, if we were going to do everything as it really is in real life things could start to be boring.");
-
- radius=prompt("By first, choose the size of the radius in meters.");
- while(radius === "" || isNaN(radius)){
-  radius=prompt("Please, type only type numbers and don't leave it blank.")
- } if(!(isNaN(radius))){
-  alert("Awesome choice!!");
+ else{
+  alert("You are boring, and it's worse than I thought!");
  }
-
-
- height=prompt("By second, choose the height of the internet cable.");
- while(height === "" || isNaN(height)){
-  height=prompt("Please, type only type numbers and don't leave it blank.")
- } if(!(isNaN(height))){
-  alert("We're getting there!");
- }
-
-
- length=prompt("By last, choose its length.");
- while(length === "" || isNaN(length)){
-  length=prompt("Please, type only type numbers and don't leave it blank.")
- } if(!(isNaN(length))){
-  alert("Soon you will see the magic number!");
- }
-
-
- var totalVolume = function (r, h, l) {
-
-  var areaCircumference = r*r*Math.PI;
-  var volume = areaCircumference*l;
-  console.log(volume);
-  return volume;
- };
-
- var resultVolume = totalVolume(radius, height, length);
-
- alert("The total volume of your own wonderful version of the Amazing Internet Cable is "+Math.round(resultVolume)+" cubic meters!!!");
-
- alert("There is definitely more than were it came from!");
-
- alert("After so hard calculations let's pretend your are going to eat an ice cream!\nThe ice cream you want to eat comes not inside a cone, but a pyramid! Let's calculate its volume! What a fun thing to do!");
-
-
-
-
- var width2=prompt("Let's start with the width of the pyramid. Type only numbers.");
-
-
-
- var length2=prompt("Now it's time for the length! Type here again. You know what you need to do.");
-
-
-
-
- var height2;=prompt("By last, type the height of your pyramid");
-
-
- function calcPyramid(w, l, h){
-  var base = w*l;
-  var volume = base*(h/3);
-  console.log(volume);
-  return volume;
- }
-
- var resultVolume2 =calcPyramid(width2, length2, height2);
-
-
 
  //DON'T FORGET TO PUT THE LAST COMMENTS!!!

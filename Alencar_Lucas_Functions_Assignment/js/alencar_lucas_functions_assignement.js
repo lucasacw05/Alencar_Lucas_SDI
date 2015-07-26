19cb01778252f678b4a173c692318079232da044
lucasacw05 <lucasacw@gmail.com>
2015-07-25 21:48:28 -0400

    First Prompt Working!
    First Prompt Working!

--- Alencar_Lucas_Functions_Assignment/js/alencar_lucas_functions_assignement.js
+++ Alencar_Lucas_Functions_Assignment/js/alencar_lucas_functions_assignement.js
@@ -7,3 +7,17 @@ Functions Assignment.
  */
 
 alert("This is Lucas Alencar's Functions Assignment. Enjoy it.");
+
+
+var firstQuestion=prompt("Did you know that the internet that you use everyday is basically a lot of giant cables overseas through all the world?");
+ while(firstQuestion === "" || !(isNaN(firstQuestion))){
+  firstQuestion=prompt("Please, answer \"yes\" or \"no\".")
+ } if(firstQuestion.toLowerCase()==="yes"){
+  alert("Great! I will do this because you want to!");
+ } else{
+  alert("It looks like you will have to do it either way!");
+ }
+
+
+
+ //DON'T FORGET TO PUT THE LAST COMMENTS!!!
